9823736ac08fb9543941572087570a968f88a26b
bulus <120481670+jqwjqhbdbv@users.noreply.github.com>
2023-02-15 00:55:39 +0800

    Update vite.config.js

--- chatgpt-web/vite.config.js
+++ chatgpt-web/vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/ChatGPT-Web/',
+  base:'/jdwjqhbdbv.github.io/',
   build:{
   },
-  // publicPath: process.env.NODE_ENV === "production" ? "/ChatGPT-Web/prod" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/jdwjqhbdbv.github.io/prod" : "/",
   resolve: {
     
     alias: {
